71fd293d1e79b23f1f7bb7058e80dee50b9b3390
Martin Flores <martin.flores@bodybuilding.com>
2018-04-30 12:30:28 -0600

    commit inicial

---
+++ src/main/java/eu/cloudscale/Request.java
@@ -0,0 +1,26 @@
+/*******************************************************************************
+*  Copyright (c) 2015 XLAB d.o.o.
+*  All rights reserved. This program and the accompanying materials
+*  are made available under the terms of the Eclipse Public License v1.0
+*  which accompanies this distribution, and is available at
+*  http://www.eclipse.org/legal/epl-v10.html
+*  
+*  @author XLAB d.o.o.
+*******************************************************************************/
+package eu.cloudscale;
+
+import java.net.URL;
+import java.util.concurrent.Callable;
+
+public class Request implements Callable<Response> {
+    private URL url;
+
+    public Request(URL url) {
+        this.url = url;
+    }
+
+    @Override
+    public Response call() throws Exception {
+        return new Response(url.openStream());
+    }
+}
---
+++ src/main/java/eu/cloudscale/Response.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+*  Copyright (c) 2015 XLAB d.o.o.
+*  All rights reserved. This program and the accompanying materials
+*  are made available under the terms of the Eclipse Public License v1.0
+*  which accompanies this distribution, and is available at
+*  http://www.eclipse.org/legal/epl-v10.html
+*  
+*  @author XLAB d.o.o.
+*******************************************************************************/
+package eu.cloudscale;
+
+import java.io.InputStream;
+
+public class Response {
+    private InputStream body;
+
+    public Response(InputStream body) {
+        this.body = body;
+    }
+
+    public InputStream getBody() {
+        return body;
+    }
+}
